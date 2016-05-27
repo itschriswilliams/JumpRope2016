c5cac3dbadb6e9ebc2cabb2e840bfd9b857fd88e
Christopher Williams <itschriswilliams@gmail.com>
2016-05-27 12:47:58 -0400

    added credibility section (HOME) and Footer (Global)

--- index.php
+++ index.php
@@ -232,13 +232,83 @@
         </div>
     </section>
 
-    <!-- Map Section -->
-    <div id="map"></div>
+    <!-- Home-Logos Section -->
+    <section id="logos" class="padded less beige">
+        <div class="container">
+            <div class="row text-xs-center text-sm-center">
+                <div class="col-xs-6 col-sm-3 center">
+                    <p><img src="//placehold.it/225x80" alt=""></p>
+                </div>
+                <div class="col-xs-6 col-sm-3 center">
+                    <p><img src="//placehold.it/225x80" alt=""></p>
+                </div>
+                <div class="col-xs-6 col-sm-3 center">
+                    <p><img src="//placehold.it/225x80" alt=""></p>
+                </div>
+                <div class="col-xs-6 col-sm-3 center">
+                    <p><img src="//placehold.it/225x80" alt=""></p>
+                </div>
+            </div>
+        </div>
+    </section>    
 
     <!-- Footer -->
-    <footer>
-        <div class="container text-center">
-            <p>Copyright &copy; Your Website 2014</p>
+    <footer id="footer">
+        <div class="backgrounded footer">
+            <div class="container">
+                <div class="row padded med">
+                    <div class="col-sm-12 text-right">
+                        <h6><a href="#page-top" class="turq-text uppercase">Back to Top</a></h6>
+                    </div>
+                </div>
+                <div class="row padded-bottom">
+                    <div class="col-xs-2 col-sm-1">
+                        <img src="img/sub/JMP-RopeGraphic.svg" alt="">
+                    </div>
+                    <div class="col-xs-5 col-sm-2">
+                        <h5 class="white-text uppercase">Social</h5>
+                        <ul class="footernav">
+                            <li><a href="#">Twitter</a></li>
+                            <li><a href="#">Facebook</a></li>
+                            <li><a href="#">Google +</a></li>
+                            <li><a href="#">LinkedIn</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-xs-5 col-sm-2">
+                        <h5 class="white-text uppercase">Company</h5>
+                        <ul class="footernav">
+                            <li><a href="#">Privacy Policy</a></li>
+                            <li><a href="#">Security & Availability</a></li>
+                            <li><a href="#">Made in NYC</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-xs-10 col-sm-4 col-xs-offset-2 col-sm-offset-0">
+                        <h5 class="white-text uppercase">The Notebook</h5>
+                        <p>Helpful tips and news from JumpRope delivered straight to your inbox.</p>
+                        <p>
+                            <form id="NotebookForm" action="#">
+                            <div class="form-group">
+                            <input type="email" class="form-control" placeholder="ENTER EMAIL HERE">
+                            <br>
+                            <input type="submit" class="btn white">
+                            </div>
+                            </form>
+                        </p>                        
+                    </div>
+                    <div class="col-xs-10 col-sm-3 col-xs-offset-2 col-sm-offset-0">
+                        <h5 class="white-text uppercase">Get in Touch</h5>
+                        <p>Connect with a seasoned expert on standards-based grading.</p>
+                        <a href="#" class="btn turq">Contact Us</a>                        
+                    </div>                    
+
+                </div>
+                <div class="row padded-bottom tiny">
+                    <div class="col-sm-12 text-center">
+                        <p class="copyright"><a href="#">Terms & Conditions</a> &nbsp; Copyright &copy; <script>document.write(new Date().getFullYear())</script> JumpRope Inc.</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </footer>
 
