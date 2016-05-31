c26409a5f45d2debc03a481f8f7ce65b0586ce9e
Christopher Williams <itschriswilliams@gmail.com>
2016-05-31 12:08:04 -0400

    restructured php setup for templating

---
+++ includes/header.php
@@ -0,0 +1,18 @@
+    <!-- Navigation -->
+    <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                    <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">
+                    <img width="150" src="img/template/JMPRP_Logo.svg" alt="">
+                </a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+                <?php include 'includes/nav.php' ?>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
---
+++ pages/home.php
@@ -0,0 +1,200 @@
+    <!-- Intro Banner -->
+    <section class="intro">
+        <div class="intro-body">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 text-left">
+                        <br><br><br>
+                        <h1>Building a better education, from the gradebook up.</h1>
+                        <p>JumpRope is more than a software company. We’re experts in standards-based grading and its practical implementation.
+</p>
+                   <!--      <a href="#about" class="btn btn-circle page-scroll">
+                            <i class="fa fa-angle-double-down animated"></i>
+                        </a> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Gradebook Plug Section -->
+    <section id="gradebook" class="text-left">
+        <div class="backgrounded gradebook">
+            <div class="container">
+                <div class="col-md-10">
+                    <h1>Deep roots in standards-based education.</h1>
+                    <p>Steeped in a profound knowledge of standards-based education, JumpRope’s gradebook does more than just track learning goals. It blends technology and instruction to elevate teacher practice and engage students.</p>
+                    <a class="btn" href="#">Explore the Gradebook</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Partnering Section -->
+    <section id="partnering" class="text-left">
+        <div class="backgrounded partnering">
+            <div class="container">
+                <div class="col-md-10">
+                    <h1>Partnering with you to effect transformational change.</h1>
+                    <p>It’s difficult to enact transformational change alone – JumpRope partners with you to make sure implementation is successful at all levels. Our site training, online courses, and expert customer service makes the transition to standards-based grading fluid not frustrating. We also provide consulting on everything from professional development, teacher practice, core philosophy, and family engagement. You get the benefit of our expertise every step of the way. </p>
+                    <a class="btn" href="#">Partner with Jumprope</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <!-- Carousel
+    ================================================== -->
+    <div id="marketing" class="carousel slide padded-top" data-ride="carousel">
+
+      <div class="carousel-inner" role="listbox">
+        <div class="item active">
+          <div class="container">
+            <div class="carousel-content">
+                <div class="row">
+                    <div class="col-sm-8 col-sm-offset-2">
+                      <p class="ltgrey-text">"At first I thought standards-based grading would be overwhelming, with me drowning in student work and standards, but it has made me a much more efficient grader since I now know exactly what to look for on an assessment."</p>
+                      <p><small class="uppercase">ANDREW KASPRISIN</small><br><span class="lato tiny">Teacher, Essex Middle School</span></p>                    
+                    </div>
+                </div>                    
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <div class="container">
+            <div class="carousel-content">
+                <div class="row">
+                    <div class="col-sm-8 col-sm-offset-2">
+                      <p class="ltgrey-text">"Standards-based grading is awesome. It has made me a much more efficient grader since I now know exactly what to look for on an assessment."</p>
+                      <p><small class="uppercase">Richard Fredriksen</small><br><span class="lato tiny">VP, Northwest High School</span></p>                    
+                    </div>
+                </div>                    
+            </div>
+          </div>
+        </div>
+      </div>
+      <a class="left carousel-control" href="#marketing" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#marketing" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div><!-- /.carousel -->
+
+    <!-- Marketing Section -->
+    <section id="marketing-whitepapers" class="text-center">
+        <div class="backgrounded beige ltgrey-text">
+            <div class="container white ltgrey-border">
+                <div class="row">
+                    <div class="col-sm-10 col-sm-offset-1">
+                        <br>
+                        <div class="row">
+                            <div class="col-sm-12 turq line">
+                                <br>
+                            </div>
+                            <div class="col-sm-12">
+                                <div class="row">
+                                    <div class="col-sm-6 padded-top med text-left">
+                                        <div class="media">
+                                          <div class="media-left media-top">
+                                            <a href="#">
+                                              <img class="media-object" src="//placehold.it/64x64" alt="...">
+                                            </a>
+                                          </div>
+                                          <div class="media-body">
+                                            <h4 class="media-heading turq-text">Implementation Benchmarks: Lorem Ipsum Dolor Sit</h4>
+                                            <small class="uppercase">Get The Whitepaper</small>
+                                          </div>
+                                        </div>                                        
+                                    </div>
+                                    <div class="col-sm-6 padded-top med text-left">
+                                        <div class="media">
+                                          <div class="media-left media-top">
+                                            <a href="#">
+                                              <img class="media-object" src="//placehold.it/64x64" alt="...">
+                                            </a>
+                                          </div>
+                                          <div class="media-body">
+                                            <h4 class="media-heading turq-text">Implementation Benchmarks: Lorem Ipsum Dolor Sit</h4>
+                                            <small class="uppercase">Get The Whitepaper</small>
+                                          </div>
+                                        </div>                                       
+                                    </div>                                    
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <br>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Who We Are Section -->
+    <section id="WhoWeAre" class="text-left">
+        <div class="backgrounded WhoWeAre">
+            <div class="container">
+                <div class="col-md-10">
+                    <h1>Experts in <br>standards-based grading.</h1>
+                    <p>JumpRope was founded by teachers who are passionate about standards-based grading. Our software tailors teacher practice to the way students learn. Built around standards-based principles, our gradebook is the most customizable on the market and works in all different learning environments. We’re facilitating a paradigm shift that empowers students, teachers, and entire school districts.</p>
+                    <a class="btn" href="#">Discover Who We Are</a>
+                </div>
+            </div>
+        </div>
+    </section>    
+
+    <!-- Blog Section -->
+    <section id="blog" class="padded">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <h4 class="ltturq-text uppercase">From the Blog</h4>
+                </div>
+            </div>
+            <div class="row text-xs-center text-sm-center">
+                <article class="col-sm-1 col-md-4">
+                    <img src="//placehold.it/300x200" alt="">
+                    <br><br>
+                    <p class="small uppercase ltgrey-text nomargin questrial">January 4, 2016</p>
+                    <p class="questrial">Suspendisse tincidunt sem tristique velit varius, quis tincidunt augue.</p>
+                </article>
+                <article class="col-sm-1 col-md-4">
+                    <img src="//placehold.it/300x200" alt="">
+                    <br><br>
+                    <p class="small uppercase ltgrey-text nomargin questrial">January 4, 2016</p>
+                    <p class="questrial">Suspendisse tincidunt sem tristique velit varius, quis tincidunt augue.</p>
+                </article>
+                <article class="col-sm-1 col-md-4">
+                    <img src="//placehold.it/300x200" alt="">
+                    <br><br>
+                    <p class="small uppercase ltgrey-text nomargin questrial">January 4, 2016</p>
+                    <p class="questrial">Suspendisse tincidunt sem tristique velit varius, quis tincidunt augue.</p>
+                </article>                                
+            </div>
+        </div>
+    </section>
+
+    <!-- Home-Logos Section -->
+    <section id="logos" class="padded less beige">
+        <div class="container">
+            <div class="row text-xs-center text-sm-center">
+                <div class="col-xs-6 col-sm-3 center">
+                    <p><img src="//placehold.it/225x80" alt=""></p>
+                </div>
+                <div class="col-xs-6 col-sm-3 center">
+                    <p><img src="//placehold.it/225x80" alt=""></p>
+                </div>
+                <div class="col-xs-6 col-sm-3 center">
+                    <p><img src="//placehold.it/225x80" alt=""></p>
+                </div>
+                <div class="col-xs-6 col-sm-3 center">
+                    <p><img src="//placehold.it/225x80" alt=""></p>
+                </div>
+            </div>
+        </div>
+    </section>  
