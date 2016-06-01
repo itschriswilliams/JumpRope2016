7711d298cacd6c0e2f1b68f0e5b4a74d6b95fe9f
Christopher Williams <itschriswilliams@gmail.com>
2016-06-01 15:01:14 -0400

    started building gradebook page

---
+++ pages/gradebook.php
@@ -0,0 +1,96 @@
+<!-- Intro Banner -->
+<section class="intro gradebook">
+    <div class="intro-body">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-7 text-left">
+                    <br>
+                    <br>
+                    <br>
+                    <h4 class="questrial turq-text uppercase margined-bottom tiny">Gradebook</h4>
+                    <h2 class="white-text">
+                        This is more than a gradebook, <br> it's a game-changer. </h2>
+                    <p class="white-text">JumpRope’s software is designed to work in all different learning environments. This flexibility allows your district to tailor our gradebook to your specific philosophy, standards, and language.</p>
+            </div>
+        </div>
+    </div>
+</div>
+</section>
+
+
+<section class="gradebook-details padded">
+    <div class="container">
+        <div class="row text-center padded-bottom less">
+            <div class="col-sml-10 col-sm1-offset-1 center">
+                <h2>JumpRope can fit into your language and philosophy</h2>
+                <p class="ltgrey-text">We recognize the process of measuring student proficiency goes by a variety of different names. Standards, competency, proficiency, mastery-based… our gradebook will work with your grading philosophy. We understand each philosophy’s nuances and support your district’s goal to measure student progress by well-defined learning objectives.</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-6 col-sm-4 text-center">
+                <img src="//placehold.it/90x90" alt="">
+                <br><br>
+                <h4>Truly Standards-Based</h4>
+                <p class="small ltgrey-text">Our software is built around the idea of creating assessments that are aligned to one or many standards.</p>
+            </div>
+            <div class="col-xs-6 col-sm-4 text-center">
+                <img src="//placehold.it/90x90" alt="">
+                <br><br>
+                <h4>Dual Reporting</h4>
+                <p class="small ltgrey-text">Optionally report  traditional grades alongside the new standard scores.</p>
+            </div>
+            <div class="col-xs-6 col-sm-4 text-center">
+                <img src="//placehold.it/90x90" alt="">
+                <br><br>
+                <h4>Student and Parent Portal</h4>
+                <p class="small ltgrey-text">Students and parents can view progress on standards, download assignments and view missing work online, improving communication among teachers, parents, and students.</p>
+            </div>
+            <div class="col-xs-6 col-sm-4 text-center">
+                <img src="//placehold.it/90x90" alt="">
+                <br><br>
+                <h4>Course and Profciency Transcripts</h4>
+                <p class="small ltgrey-text">Offers the complete picture of a student’s progress, work habits, and educational history.</p>
+            </div>            
+            <div class="col-xs-6 col-sm-4 text-center">
+                <img src="//placehold.it/90x90" alt="">
+                <br><br>
+                <h4>Habits of Work </h4>
+                <p class="small ltgrey-text">Gives teachers the ability to separately assess and report out non-academic standards.</p>
+            </div>
+
+            <div class="col-xs-6 col-sm-4 text-center">
+                <img src="//placehold.it/90x90" alt="">
+                <br><br>
+                <h4>Behavior Management and Response to Intervention</h4>
+                <p class="small ltgrey-text"> Allows staff to become instantly familiar with the behavior history of a student, making it easy to spot trends. Our anecdotal and narrative feedback system facilitates communication about student behavior among staff.</p>
+            </div>                                                            
+         
+        </div>
+    </div>
+</section>
+
+<section class="gradebook-whitepaper">
+    <div class="container">
+        <div class="row flex">
+            <div class="col-sm-6 ltturq padded all productBar">
+                <h5 class="questrial turq-text uppercase margined-bottom tiny">CALCULATION METHODS AND CUSTOM REPORTING</h5>
+                <h4 class="white-text questrial">With deep roots in standards-based teaching and learning philosophy, our gradebook creates reports personalized for your students. </h4>
+                <p class="small white-text lato">Built from the ground up as a standards-based gradebook, JumpRope understands what your grading goals are. No more fighting with traditional gradebooks or trying to fit a square peg into a round hole. It’s a powerful tool that identifies challenges, furthers instructional goals, and shifts teacher practice.</p>
+            </div>
+            <div class="col-sm-6 ltturq padded all sqimg gradebook1">
+                &nbsp;
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="productBar">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4 padded tiny">
+                <a href="#"><h6 class="uppercase white-text right"><img src="//placehold.it/64x64" alt="">Learn About The Curriculum Planning Tool</h6></a>
+            </div>
+        </div>        
+    </div>
+</section>
