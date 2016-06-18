998472c3529aded2e1f07d02ef1f60aabe0099a0
Christopher Williams <itschriswilliams@gmail.com>
2016-06-17 21:08:24 -0400

    blog page initial build

---
+++ pages/blog.php
@@ -0,0 +1,73 @@
+<!-- Intro Banner -->
+<section class="intro blog">
+    <div class="container white margined-top more">
+        <div class="row">
+            <div class="col-xs-10 col-xs-offset-1 margined">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <a href="#">
+                            <img class="ltgrey" src="http://placehold.it/505x335" width="505" height="335" alt=""></a>
+                    </div>
+                    <div class="col-sm-6 text-left">
+                        <h5 class="questrial turq-text uppercase margined-bottom margined-top less">JANUARY 4, 2016</h5>
+                        <h2>
+                            <a class="grey-text questrial" href="#">
+                                Professional Learning Doubleheader: ConnectEd Coast & EdCamp WME
+                            </a>
+                        </h2>
+                        <p class="grey-text">
+                            by
+                            <a href="#" class="blue-text">Becca Redman</a>
+                        </p>
+                        <a href="#" class="btn turq">Keep Reading</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="notebook-signup ltgrey">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <h5 class="questrial turq-text uppercase margined-top less">The Notebook</h5>
+                <p class="small grey-text">
+                    Get helpful tips and news from JumpRope delivered straight to your inbox.
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="blogList">
+
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-10 col-xs-offset-1 margined">
+                <article>
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <a href="#">
+                                <img class="ltgrey" src="http://placehold.it/505x335" width="505" height="335" alt=""></a>
+                        </div>
+                        <div class="col-sm-8 text-left">
+                            <h5 class="questrial turq-text uppercase margined-bottom margined-top less">JANUARY 4, 2016</h5>
+                            <h2>
+                                <a class="grey-text questrial" href="#">
+                                    Professional Learning Doubleheader: ConnectEd Coast & EdCamp WME
+                                </a>
+                            </h2>
+                            <p class="grey-text">
+                                by
+                                <a href="#" class="blue-text">Becca Redman</a>
+                            </p>
+                            <a href="#" class="btn turq">Keep Reading</a>
+                        </div>
+                    </div>
+                </article>
+            </div>
+        </div>
+    </div>
+
+</section>
