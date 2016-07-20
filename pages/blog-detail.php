cd98a9532b4dd68e6f82367e71bb088eaf363e2f
Christopher Williams <itschriswilliams@gmail.com>
2016-07-20 17:17:43 -0400

    blog detail cleaning

---
+++ pages/blog-detail.php
@@ -0,0 +1,54 @@
+<section class="blog-detail padded gridded blog">
+    <div class="container white margined-top padded-top">
+        <div class="row">
+            <div class="col-sm-7 col-sm-offset-1">
+                <article class="blog-post-detail">
+                    <h5 class="questrial grey-text uppercase margined-bottom tiny">January 4, 2016</h5>
+                    <h2 class="grey-text questrial">
+                            Suspendisse tincidunt sem tristique velit varius, quis tincidunt augue.
+                    </h2>
+                    <h4 class="grey-text byline">
+                        by
+                        <a href="#" class="blue-text">Becca Redman</a>
+                    </h4>
+                    <a href="#" class="share uppercase">Share</a>
+                    <div class="post-image">
+                        <img src="http://placehold.it/700x450" alt="">
+                        <p class="small caption">The above image is from the ConnectEd Coast Kickoff provided by @mistercmaine.</p>
+                    </div>
+                    <div class="post-body">
+                        <p>The start of a new year kicks off a fresh cycle of annual teacher-driven professional development events here in Maine. I look forward to this series of weekend conferences spent engaged in dialogue with colleagues from across the state. This is a time to connect “in real life” with members of my valued digital professional learning network (PLN). These face-to-face professional exchanges establish new ties and strengthen pre-existing collaborative relationships. Together, we share our successes and troubleshoot challenges collectively, and keep the conversations going through the power of connectivity.</p>
+
+                        <p><a href="#">ConnectEd Coast</a> debuted at <a href="#">South Portland High School</a> on January 30th. A collaboration between the South Portland, Cape Elizabeth and Old Orchard Beach school departments (three districts that share a coastline, hence the clever name), ConnectEd Coast brought together nearly 200 participants for a “modified EdCamp-style” day of learning. In addition to the traditional attendee-designed day-of sessions, ConnectEd Coast also offered participants formal, predetermined sessions on topics including Micro-credentials & Badging, Improv Mindset & Tech Integration and Flexible Learning Environments. In between sessions, participants could attend quick, energy-infused lectures dubbed “Coast Chats” (evocative of ISTE Ignite presentations).</p>
+
+                        <p>What made ConnectED Coast outstanding was the range of topics & presenters: from tech “how-tos” facilitated by MLTI, a panel featuring the student tech team from Pond Cove Elementary School, or informal discussions about mindset and transforming assessment practices. Perhaps one of the most inspiring sessions I attended was geared towards administrators, who engaged in a thoughtful conversation regarding how to best support teachers as we work to implement a proficiency-model and strive to encourage educators’ “life-long learning,” whether that is through access to new technology or responsive professional development. At the end of the day, I felt profoundly inspire and ready to bring new ideas to my broader learning community.</p>
+
+                        <p>The following weekend, I attended EdCamp Western Maine at Mt. Blue Campus (Farmington, ME). Building upon last year’s momentum, nearly 100 educators from nearby districts and as far away as Fort Kent (for those of you who are unfamiliar with Maine geography, Fort Kent is on the northernmost border with Canada-nearly five hours from the EdCampWME host)!</p>
+
+                        <p>After indulging in coffee and (amazing) doughnuts provided by the culinary arts students from the Foster Career and Technical Education Center, we collectively built a schedule that highlights the variety of attendee’s professional passions and curiosities, including Standards Alignment, Evaluating Formative Apps, Sketchnotes and Design Thinking. As an organizer, I bounced between sessions - taking time to experience Google Cardboard with Alice Barr and explore the impromptu Makerspace in the commons. There, I tinkered with circuitry using a LittleBits kit, figured out how to use Osmo’s technology (so cool!) and was reminded that creators of any age can become delightfully lost in a fish-tub full of Legos.</p>
+
+                        <p>At both ConnectEd Coast and EdcampWME, I attended sessions on teaching in a proficiency-based mode (once as a moderator, again as an observer). In both sessions, participants included educators from all corners of the field: practicing K-12 teachers, administrators, higher education professors & many pre-service teachers. It was evident that, regardless of position, there is a resounding embracement of the principles behind proficiency-based education. Teachers want to establish a model that is supportive of all students, rooted in the power of authentic “I can” achievements and devoid of punitive compliance culture.</p>
+
+                        <p>As we in Maine continue to implement a statewide proficiency model, it’s clear that these sometimes-tricky discussions should be on-going and extend beyond individual districts. I’m always impressed by the number of passionate, thoughtful educators who attend optional, Saturday professional development days like ConnectED Coast or EdCampWME. Our strength in numbers is bolstered by our capacity to remain digitally connected as we continue this challenging but very important work.</p>                        
+                    </div>
+                    <div class="post-author">
+                        <div class="row">
+                            <div class="col-sm-3">
+                                <img src="http://placehold.it/130x130" alt="">
+                            </div>
+                            <div class="col-sm-8">
+                                <h4 class="grey-text">About Becca Redman</h4>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris maximus eros tristique, pretium risus vel, facilisis ligula. Sed dapibus aliquet velit, a facilisis magna dignissim non. </p>
+                                <p><a href="#">View all posts from Becca Redman &rarr;</a></p>                                      
+                            </div>
+                        </div>
+                    </div>
+
+                </article>
+            </div>
+            <div class="col-sm-2 col-sm-offset-1">
+
+            </div>            
+        </div>
+    </div>
+</section>
