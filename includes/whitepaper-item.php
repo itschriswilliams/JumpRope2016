ff1a03c329abe339f972d7a5bfd8d31f854a1805
Christopher Williams <itschriswilliams@gmail.com>
2016-07-29 10:30:06 -0400

    resources page

---
+++ includes/whitepaper-item.php
@@ -0,0 +1,24 @@
+        <article class="row whitepaper-list">
+            <div class="col-xs-10 col-xs-offset-1">
+                <div class="row margined">
+                    <div class="col-sm-4">
+                        <a href="#">
+                            <img class="ltgrey" src="http://placehold.it/505" alt=""></a>
+                    </div>
+                    <div class="col-sm-8 text-left">
+                        <h5 class="questrial ltgrey-text uppercase margined-bottom tiny">JANUARY 4, 2016</h5>
+                        <h2>
+                            <a class="grey-text questrial" href="#">
+                                Suspendisse tincidunt sem tristique velit varius, quis tincidunt augue.
+                            </a>
+                        </h2>
+                        <p class="grey-text">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tincidunt sem tristique velit varius, quis tincidunt augue ornare.
+                        </p>
+                        <a href="#" class="share uppercase">Share</a>
+                        <a href="#" class="btn grey">Download the Whitepaper</a>
+                    </div>
+                </div>
+                <hr>
+            </div>
+        </article>
--- pages/resources.php
+++ pages/resources.php
@@ -2,41 +2,21 @@
 <section class="intro blog">
     <div class="container white margined-top more">
         <div class="row">
-            <div class="col-xs-10 col-xs-offset-1 margined">
-                <div class="row">
-                    <div class="col-sm-6">
-                        <a href="#">
-                            <img class="ltgrey" src="http://placehold.it/505" alt=""></a>
-                    </div>
-                    <div class="col-sm-6 text-left">
-                        <h5 class="questrial turq-text uppercase margined-bottom margined-top less">JANUARY 4, 2016</h5>
-                        <h2>
-                            <a class="grey-text questrial" href="#">
-                                Professional Learning Doubleheader: ConnectEd Coast & EdCamp WME
-                            </a>
-                        </h2>
-                        <p class="grey-text">
-                            by
-                            <a href="#" class="blue-text">Becca Redman</a>
-                        </p>
-                        <a href="#" class="btn turq">Keep Reading</a>
-                    </div>
-                </div>
+            <div class="col-sm-10 col-sm-offset-1 centered">
+                <h2 class="grey-text questrial margined tiny padded">Whitepapers</h2>
+                <hr>
             </div>
         </div>
+        <?php include 'includes/whitepaper-item.php' ?>
     </div>
 </section>
 
-<section class="blog-wrap margined-top">
+<section class="blog-wrap margined">
     <div class="container">
-        <div class="row">
-            <div class="col-xs-10 col-xs-offset-1">
-                <?php include 'includes/blogPost-List.php' ?>
-                <?php include 'includes/blogPost-List.php' ?>
-                <?php include 'includes/blogPost-List.php' ?>
-                <?php include 'includes/blogPost-List.php' ?>
-                <?php include 'includes/blogPost-List.php' ?>
-            </div>
-        </div>
+        
+                <?php include 'includes/whitepaper-item.php' ?>
+                <?php include 'includes/whitepaper-item.php' ?>
+                <?php include 'includes/whitepaper-item.php' ?>
+                
     </div>
 </section>
